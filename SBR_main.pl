53cb61bc7063f1c224704092bb8a10d4fb1e670b
Stefan Badelt <stef@trieb.tbi.univie.ac.at>
2009-06-16 17:23:35 +0200

    Assign Cars to start postition

--- SBR_main.pl
+++ SBR_main.pl
@@ -3,39 +3,42 @@
 use strict;
 use warnings;
 use Data::Dumper;
-use vars qw/@RaceTrack %STATE $L/;
-
+use vars qw/@RaceTrack %CARS/;
 
 
 @RaceTrack = readin_textfile();
 #   print Dumper(@RaceTrack);
 
+%CARS = ( Fab => undef,
+	  Stef => undef,
+	  Test => undef,);
+
+# Find possible Start Positions, find End positions
+my @CurrentTrack = process_matrix(\@RaceTrack, \%CARS);
 
 
+sub process_matrix {
+    my @newtrack = @{shift()};
+    my %cars = %{shift()};
 
-## ------------------------------------------
-# Car-Subroutines must return their Position.
+    foreach my $car (keys %cars) { 
+       if (defined($cars{$car})) {
+          print "strange\n";
+       } else {
+          my $count=0;
+          foreach (@{$newtrack[-1]}) {
+             if ($_ eq 1) {
+                 $newtrack[-1]->[$count] = substr($car, 0, 1);
+                 last;
+             }
+             $count++;
+          }
+       }
+    }
 
-while ($L == 1) {
-
- $STATE{car} = car();
- $L = checkFinish(@{$STATE{car}}[0]);
-
+ print "@{$newtrack[-1]}\n";
+ return @newtrack;
 }
-
-
-
-
-
-sub car {
-my $pos = shift();
-
-
-return $pos;
-}
-
-
-
 
 
 
@@ -50,14 +53,10 @@ sub readin_textfile {
  return @track;
 }
 
-sub checkFinish {
-	my ($x, $y) = @_;
-	$L = 1 if ($x == '1');
-	}
+sub car {
+    my @matrix = @{shift()};
+    my ($pos, $kmh) = @_;
+ # Process position and kmh to generate the new value
+ return $pos, $kmh;
+}
 
-sub start {
-	my @st_pos;
-	foreach my $k (0 .. $#{$RaceTrack}[-1]) {
-		if (${$RaceTrack}[$k] == 1) {
-			push(@st_pos, [ 
-	
